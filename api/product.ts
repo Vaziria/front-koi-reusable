d954a08e76a5400d392f96feaf5189148a98cf36
HFrada <madefrada@gmail.com>
2021-04-15 09:38:20 +0700

    daterange type

--- api/product.ts
+++ api/product.ts
@@ -17,7 +17,15 @@ export interface IkanFilter {
   offset?: number
 }
 
+function cleanparams<T> (params: IkanFilter): T {
+  const data = Object.fromEntries(Object.entries(params).filter(([_, v]) => {
+    return !((v === undefined) || (v === null) || (v === 0) || (v === []) || (v === ''))
+  }))
+  return data as T
+}
+
 export async function publicIkan (params?: IkanFilter): Promise<IIkan[]> {
+  params = cleanparams<IkanFilter>(params || {})
   const res = await client.get('/public/ikan', {
     params
   })
---
+++ typing/vue2-daterange-picker/index.d.ts
@@ -0,0 +1,4 @@
+declare module 'vue2-daterange-picker' {
+  interface ComponentCustomProperties {
+  }
+}
