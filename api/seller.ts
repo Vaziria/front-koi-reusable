f0d98592d141d3178b59e36861309281485adbf2
Heri <manorder123@gmail.com>
2021-04-15 14:59:28 +0700

    seller api

--- api/seller.ts
+++ api/seller.ts
@@ -1,9 +1,22 @@
+import { Seller } from '@/reusable/model/seller'
 import client, { StdRes } from './client'
 
 export interface AddSellerPayload {
     type: 'phone' | 'email'
     user: string
     pass: string
+}
+
+export async function findSeller (params: { q: string }): Promise<{ data: Seller[] }> {
+  const res = await client.get('/internal/sellers', {
+    params
+  })
+  return res.data
+}
+
+export async function getSeller (id: string): Promise<Seller> {
+  const res = await client.get(`/shop/${id}`)
+  return res.data
 }
 
 export async function addSeller (payload: AddSellerPayload): Promise<StdRes> {
---
+++ model/seller.ts
@@ -0,0 +1,20 @@
+/* eslint-disable camelcase */
+export interface Seller {
+    id: string
+    city: string
+    ikan_count: number
+    ikan_limit: number
+    last_stat_up: number
+    like_count: number
+    location: string
+    order_count: number
+    phone: string
+    profile_image: string
+    region: string
+    review_count: number
+    seller_name: string
+    transaction: number
+    created: number
+    state: string
+    photoUrl: string
+}
