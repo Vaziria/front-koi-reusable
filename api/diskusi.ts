7211a84b4fd85996d4fef301f83fe8af8cab4c25
Heri <manorder123@gmail.com>
2021-04-26 16:29:20 +0700

    addd typing dan model diskusi

---
+++ api/diskusi.ts
@@ -0,0 +1,10 @@
+import { db } from '@/utils/firebase'
+import { Diskusi } from '../model/diskusi'
+
+function createDiskusiRef(shopid: string){
+
+}
+
+export async function getDiskusi (shopid: string): Promise<Diskusi[]> {
+  return []
+}
