6389b7be44a4ab4ffacf3bf6f55d559448b470fd
Heri <manorder123@gmail.com>
2021-05-06 14:00:24 +0700

    api stat

---
+++ api/stat.ts
@@ -0,0 +1,39 @@
+/* eslint-disable camelcase */
+import { StatusOrder } from '../model/order'
+import client from './client'
+
+export interface StandartFilter {
+    status?: string
+    q?: string
+    datemin?: number
+    datemax?: number
+    pay_status?: string
+}
+
+export type StatRes = {
+  [ key in StatusOrder | 'key' ]: key extends 'key' ? string: number
+}
+
+export async function generalStat (payload: StandartFilter): Promise<StatRes[]> {
+  const res = await client.get('/seller/stat/general', { params: payload })
+  const datas = res.data.data
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const hasil = datas.map((data: any) => {
+    const statuses: {
+      [k in StatusOrder]?: number
+    }[] = data.status || []
+
+    const fix = {
+      key: Object.keys(data)[0]
+    }
+
+    statuses.map(status => {
+      Object.assign(fix, status)
+    })
+
+    return fix
+  })
+
+  return hasil
+}
