8fdc31e8e1adabb0b8e45e779b16f8b863e6e6d6
HFrada <madefrada@gmail.com>
2021-07-26 17:58:17 +0700

    optional name & ssr initial state type

--- navigation/types.ts
+++ navigation/types.ts
@@ -1,6 +1,6 @@
 import { RouteConfig } from 'vue-router'
 
-export type RouteConfiguration<T> = RouteConfig & { name: keyof T, displayName?: string, hidden?: boolean }
+export type RouteConfiguration<T> = RouteConfig & { name?: keyof T, displayName?: string, hidden?: boolean }
 
 export type ChildNameSpaced<T, N extends string> = {
   [P in keyof T & string as `${N}_${P}`]: T[P]
--- typing/index.d.ts
+++ typing/index.d.ts
@@ -3,5 +3,6 @@ import firebase from 'firebase'
 declare global {
     interface Window {
         recaptchaVerifier: firebase.auth.RecaptchaVerifier
+        __INITIAL_STATE__: any
     }
 }
