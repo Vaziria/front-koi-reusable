2aa2b8b085bd7c48c6d147b3ea16759020af7e40
HFrada <madefrada@gmail.com>
2021-07-27 17:52:25 +0700

    kategori api

--- api/shop.ts
+++ api/shop.ts
@@ -1,7 +1,12 @@
 import { Seller } from '../model/seller'
 import client from './client'
 
-export async function listShop (query: unknown): Promise<Seller> {
+export type Category = {
+  key: string
+  doc_count: number
+}
+
+export async function listShop (query: unknown): Promise<Seller[]> {
   const req = await client.get('/shop/list', { params: query })
   return req.data
 }
@@ -9,4 +14,16 @@ export async function listShop (query: unknown): Promise<Seller> {
 export async function getShop (id: string): Promise<Seller> {
   const req = await client.get('/shop/' + id)
   return req.data.data
+}
+
+export async function getShopCat (seller_id: string): Promise<Category[]> {
+  const params = { seller_id }
+  const req = await client.get('public/shop/category', { params })
+
+  return req.data.map((item) => {
+    return {
+      key: item.key,
+      name: item.key.charAt(0).toUpperCase() + item.key.slice(1)
+    }
+  })
 }
