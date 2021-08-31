b0c1a1d948bda976a26e6b2c3fcc4914e128cda7
Henn <madefrada@gmail.com>
2021-08-31 13:11:53 +0700

    seller rekening

--- api/seller.ts
+++ api/seller.ts
@@ -1,3 +1,5 @@
+/* eslint-disable camelcase */
+import firebase from 'firebase'
 import { Seller } from '../model/seller'
 import client, { StdRes } from './client'
 
@@ -5,6 +7,12 @@ export interface AddSellerPayload {
     type: 'phone' | 'email'
     user: string
     pass: string
+}
+
+export type Rekening = {
+  bank_name: string
+  rek_number: string
+  rek_name: string
 }
 
 export async function findSeller (params: { q: string }): Promise<{ data: Seller[] }> {
@@ -22,4 +30,40 @@ export async function getSeller (id: string): Promise<Seller> {
 export async function addSeller (payload: AddSellerPayload): Promise<StdRes> {
   const res = await client.post('/internal/seller', payload)
   return res.data
+}
+
+export function sellerGeneral (sellerid: string): firebase.firestore.CollectionReference<firebase.firestore.DocumentData> {
+  return firebase
+    .firestore()
+    .collection('Sellers')
+    .doc(sellerid)
+    .collection('general')
+}
+
+export async function getRekening (sellerid: string): Promise<Rekening> {
+  const rekening = await sellerGeneral(sellerid)
+    .doc('rekening')
+    .get()
+
+  if (rekening.exists) {
+    return rekening.data() as Rekening
+  }
+
+  return {
+    bank_name: '',
+    rek_number: '',
+    rek_name: ''
+  }
+}
+
+export async function setRekening (sellerid: string, payload: Rekening): Promise<void> {
+  const general = sellerGeneral(sellerid)
+  const rekening = general.doc('rekening')
+  const rekeningExist = (await rekening.get()).exists
+
+  if (rekeningExist) {
+    await rekening.update(payload)
+  } else {
+    await rekening.set(payload)
+  }
 }
