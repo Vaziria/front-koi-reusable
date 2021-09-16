a444ffd8085f3d3d1b7ade9474e582a486b2b992
Heri <manorder123@gmail.com>
2021-09-16 13:16:21 +0700

    model notif

--- model/notifs.ts
+++ model/notifs.ts
@@ -1,4 +1,4 @@
-import { Chat } from './Chat'
+import { Chat } from './chat'
 
 const orderNotifType = ['auto_cancel_order', 'cancel_order', 'new_order', 'unverify_paid', 'selesai', 'submit_cancel_order', 'titip_order', 'confirm_order', 'process_order', 'karantina_order', 'dikirim'] as const
 
@@ -11,7 +11,6 @@ const basicNotifType = ['system', 'new_chat', 'diskusi', 'auto_cancel_order', 'c
 const sellerNotifType = ['new_order', 'unverify_paid', 'selesai', 'submit_cancel_order'] as const
 const buyerNotifType = ['promo', 'ikan', 'titip_order', 'confirm_order', 'process_order', 'karantina_order', 'dikirim'] as const
 
-
 export type BasicNotifType = typeof basicNotifType[number]
 export type SellerNotifType = typeof sellerNotifType[number] | BasicNotifType
 export type BuyerNotifType = typeof buyerNotifType[number] | BasicNotifType
@@ -19,17 +18,20 @@ export type BuyerNotifType = typeof buyerNotifType[number] | BasicNotifType
 export interface INotifIkan {
   id: string
   type: 'ikan'
+  // eslint-disable-next-line camelcase
   ikan_id: string
+  // eslint-disable-next-line camelcase
   ikan_name: string
   image: string
   category: string
   price: number
   ukuran: number
+  // eslint-disable-next-line camelcase
   seller_name: string
   unread: boolean
   created: number
 
-} 
+}
 
 export interface INotifOrder {
   id: string
@@ -53,6 +55,7 @@ export interface INotifChat extends Chat {
 export interface INotifDiskusi {
   id: string
   type: 'diskusi'
+  // eslint-disable-next-line camelcase
   ikan_id: string
   shopid: string
   userid: string
