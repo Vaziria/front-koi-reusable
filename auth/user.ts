c237ea3477ca4d11f4d343eb8c9d851023ac3f76
Henn <madefrada@gmail.com>
2021-09-25 12:38:31 +0700

    setup notif

--- auth/user.ts
+++ auth/user.ts
@@ -4,13 +4,14 @@ import { UserStore } from '../store/user'
 import { db, realdb } from '../../utils/firebase'
 import { Access } from '../model/access'
 import { ChatStore } from '../store/chat'
-// import { subChat } from '@/firedb/chat'
+import { setupNotification } from '../notification'
+import { NotifStore } from '../store/notif'
+import { specialLog } from '../utils/logger'
 
-type Store = UserStore & ChatStore
+type Store = UserStore & ChatStore & NotifStore
 
 export async function getAccess (uid: string): Promise<Access | null> {
   const doc = await db.collection('Access').doc(uid).get()
-  // console.log('uid', uid, doc.data())
   if (doc.exists) {
     const data = doc.data()
     const access: Access = data as Access
@@ -33,14 +34,25 @@ export class AuthUser {
     return await this.auth.signOut()
   }
 
+  async setupNotification (userid: string): Promise<void> {
+    await setupNotification((notif) => {
+      const selfChat = notif.type === 'new_chat' && notif.from_id === userid
+      const selfDiskusi = notif.type === 'diskusi' && notif.userid === userid
+      if (selfChat || selfDiskusi) {
+        return
+      }
+
+      this.store.commit('notif/push_new_notif', notif)
+    })
+  }
+
   async userStateChanged (user: firebase.User | null): Promise<void> {
     if (user) {
-      if (process.env.NODE_ENV !== 'production') console.log('data_user:', user)
-      console.log('logining user')
+      specialLog('logining user', user)
 
       await this.store.dispatch('user/login', user)
+      await this.setupNotification(user.uid)
       setupPresence(user.uid, db, realdb)
-      // subChat(user.uid)
     } else {
       this.store.commit('user/set_logout')
       this.store.commit('chat/reset_msg')
