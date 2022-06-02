38273550c8712ef7130cb108bae78906d661d294
Henn <madefrada@gmail.com>
2022-06-02 10:16:15 +0700

    env

--- api/client.ts
+++ api/client.ts
@@ -1,9 +1,26 @@
 import axios from 'axios'
 
+const {
+  VUE_APP_API_BASE,
+  VUE_APP_API_ELASTIC,
+  VUE_APP_ELASTIC_USER,
+  VUE_APP_ELASTIC_PASS
+} = process.env
+
 const client = axios.create({
-  baseURL: process.env.VUE_APP_API_BASE,
+  baseURL: VUE_APP_API_BASE,
   // withCredentials: true,
   timeout: 120000
+})
+
+export const elasticClient = axios.create({
+  baseURL: VUE_APP_API_ELASTIC,
+  timeout: 120000,
+  withCredentials: true,
+  auth: {
+    username: VUE_APP_ELASTIC_USER,
+    password: VUE_APP_ELASTIC_PASS
+  }
 })
 
 export default client
